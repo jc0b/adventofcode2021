50bb4eca23d44be06eb707a398cb6b71f0631d4d
jc0b <j@jc0b.computer>
2021-12-10 14:19:32 +0100

    Finished day 7

---
+++ day7/day_seven.py
@@ -0,0 +1,37 @@
+def part_one():
+	input = open('input.txt', 'r')
+	Lines = input.readlines()
+
+	crab_horizontal = [int(x) for x in Lines[0].strip().split(",")]
+	max_crab = max(crab_horizontal)
+	smallest_sum = 10000000000000
+	for i in range(0, max_crab):
+		movement_sum = 0
+		for j in crab_horizontal:
+			movement_sum += abs(j - i)
+		if movement_sum < smallest_sum:
+			smallest_sum = movement_sum
+	return smallest_sum
+
+def part_two():
+	input = open('input.txt', 'r')
+	Lines = input.readlines()
+
+	crab_horizontal = [int(x) for x in Lines[0].strip().split(",")]
+	max_crab = max(crab_horizontal)
+	smallest_sum = 10000000000000
+	for i in range(0, max_crab):
+		movement_sum = 0
+		for j in crab_horizontal:
+			movement_sum += (abs(j - i) + 1) * abs(j - i) * 0.5
+		if movement_sum < smallest_sum:
+			smallest_sum = movement_sum
+	return int(smallest_sum)
+
+if __name__ == '__main__':
+	print(part_one())
+	print(part_two())
+
+
+## for every possible movement value
+## 
